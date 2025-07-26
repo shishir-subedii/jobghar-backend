8abc66b9ce2c103a8ae91fd6a40a9dc1637bd9fb
Shishir <shishir16subedi@gmail.com>
2025-07-26 15:26:55 +0545

    Change Db to Postgres

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,3 +1,27 @@
+// import { Module } from '@nestjs/common';
+// import { ConfigService } from '@nestjs/config';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+
+// @Module({
+//     imports: [
+//         TypeOrmModule.forRootAsync({
+//             useFactory: async (configService: ConfigService) => ({
+//                 type: 'postgres',
+//                 host: configService.get<string>('DB_HOST'),
+//                 port: configService.get<number>('DB_PORT'),
+//                 username: configService.get<string>('DB_USERNAME'),
+//                 password: configService.get<string>('DB_PASSWORD'),
+//                 database: configService.get<string>('DB_NAME'),
+//                 autoLoadEntities: true,
+//                 synchronize: true, // ❗Set to false in production and use migrations instead
+//             }),
+//             inject: [ConfigService],
+//         }),
+//     ],
+// })
+// export class DatabaseModule { }
+
+
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -6,18 +30,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     imports: [
         TypeOrmModule.forRootAsync({
             useFactory: async (configService: ConfigService) => ({
-                type: 'mysql',
+                type: 'postgres',
                 host: configService.get<string>('DB_HOST'),
                 port: configService.get<number>('DB_PORT'),
                 username: configService.get<string>('DB_USERNAME'),
                 password: configService.get<string>('DB_PASSWORD'),
                 database: configService.get<string>('DB_NAME'),
                 autoLoadEntities: true,
-                synchronize: true,
+                synchronize: true, // ❗Disable this in production and use migrations
+                ssl: {
+                    rejectUnauthorized: false, // Required by Neon
+                },
             }),
             inject: [ConfigService],
         }),
-
     ],
 })
 export class DatabaseModule { }
